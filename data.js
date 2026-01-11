041f4375dc65f5427cd017ac8b7dc1a2d34b7334
ocean <ocean@ocean.lol>
2026-01-11 00:55:09 +0000

    i may be schewpidd

--- data.js
+++ data.js
@@ -1130,7 +1130,7 @@ var images = [
         "title": "pregnantjellyg.png",
         "author": "anon",
         "date": "28 Sep 2025 15:00:00 GMT+1",
-        "comment": "",
+        "comment": "<a href='https://pbs.twimg.com/media/G1nlRR7WYAAI1FA?format=png&name=large'>original</a>",
     },
     {
         "filename": "content.png",
@@ -1144,7 +1144,7 @@ var images = [
         "title": "words collide",
         "author": "ocean",
         "date": "29 Sep 2025 22:19:00 GMT+1",
-        "comment": "<a href='https://pbs.twimg.com/media/G1nlRR7WYAAI1FA?format=png&name=large'>original</a>",
+        "comment": "",
     },
     {
         "filename": "pyramid.png",
